a9e9c504b931ec8c0cea3ba8e6510c8a97df97c0
Chuong Nguyen <32564488+chvng@users.noreply.github.com>
2018-08-21 16:15:48 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,30 @@
 /**
  * @author Chuong N.
  */
+
+$(document).ready(function() {
+    let $results = $('#results');  
+    let $category = $(".category");
+
+    $.getJSON('../js/reference.json', function(data) {
+        console.log(data); // testing if this works
+        
+        $(data).each(function (i, reference) {
+            $results.append(showResults(reference))
+        });
+    });
+
+
+    /**
+     * This function shows the output to HTML with the data from
+     * JSON file {reference.json}.
+     */
+    function showResults(result) {
+        return `
+        <div class="col-3">
+            ${result.name}
+        </div>
+        `
+    }
+
+});
