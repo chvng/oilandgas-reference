7a6043c231447b48e57bd132e4620c28014f9934
Chuong Nguyen <32564488+chvng@users.noreply.github.com>
2018-08-29 13:36:20 +0200

    Update app.js
    Working code with append() and slightly animation.

--- js/app.js
+++ js/app.js
@@ -5,89 +5,97 @@
 $(document).ready(function () {
     // Variables
     let $results = $('#results');
+    let $segment = $('#segment');
+    let $scope = $('#scope');
 
     /**
      * Fetching JSON from the file..
      * REMINDER: Replace ["../references.json"] with provided link from Sitefinity when going online.
      */
     $.getJSON("../references.json", function (data) {
-        htmlJSON(data);
-
+        showAll(data);
+        selectedSegment(data);
+        selectedScope(data);
     });
 
+    function selectedScope(data) {
+        $scope.change(function() {
+            $results.empty();
+            $.each(data, function(i, ref) {
+                $.each(ref.scope, function(j, scope) {
+                    if($scope.val() === scope) {
+                        console.log(ref.name+" -- "+ref.scope); // Test
+                        appendJSON(ref);
+                    }
+                }); 
+            });
+        });
+    }
+
     function selectedSegment(data) {
-        let $segment = $('#segment');
-        $.each(data, function (i, ref) {
-            $segment.change(function () {
-                if ($(this).val() === ref.segment) {
-                    console.log(ref.name);
-                    htmlJSON(data, ref.segment);
+        $segment.change(function() {
+            $results.empty();
+            $.each(data, function(i, ref) {
+                if($segment.val() === ref.segment) {
+                    console.log(ref.name+" -- "+ref.segment); // Test
+                    appendJSON(ref);
                 }
             });
         });
     }
 
+    function showAll(data) {
+        $.each(data, function(i, ref) {
+            appendJSON(ref);
+        });
+    }
+
+    /**
+     * This function helps with appending JSON objects to HTML. 
+     */
+    function appendJSON(data) {
+        let output = (`
+            <a href=${data.link}>
+                <div class="col-3 col-3--md">
+                    <div class="container__image">
+                    <img src=${data.image} class="img--resize">
+                </div>
+                <div class="container__text">
+                    <span class="container__text--header">${data.name}</span>
+                    <p>${data.description}</p>
+                </div>
+            </div>
+            </a>
+        `);
+        // For some slightly fancy animation
+        $(output).hide().appendTo($results).fadeIn(500);
+    }
+
     /**
      * Passing the JSON data to HTML page.
-     */
-    function htmlJSON(data, category) {
+     * Not sure if this will be needed anymore - keeping it anyways.
+    function htmlJSON(data) {
         let output = [];
 
-        // Running through the data and push.
+        // Going through for-loop to get the individual objects.
         for(let i = 0; i < data.length; i++) {
             output.push(`
             <a href=${data[i].link}>
-                <div class="col-3 col-3--md"
-                    <div class="placeholder">
-                        <div class="container__image">
-                            <img src=${data[i].image} class="img--resize">
-                        </div>
-                        <div class="container__text">
-                            <span class="container__text--header">${data[i].name}</span>
-                            <p>${data[i].description}</p>
-                        </div>
+                <div class="col-3 col-3--md">
+                    <div class="container__image">
+                        <img src=${data[i].image} class="img--resize">
+                    </div>
+                    <div class="container__text">
+                        <span class="container__text--header">${data[i].name}</span>
+                        <p>${data[i].description}</p>
                     </div>
                 </div>
             </a>
             `);
         }
-        // Showing the output in HTML.
-        $results.html(output);
+        // Output to HTML.
+        $results.html(output.join(''));
     }
+    */
 
-    function ihtmlJSON(data, category) {
-        let newHTML = [];
-
-        for (let i = 0; i < data.length; i++) {
-            if(data[i].segment === category)
-            newHTML.push(`
-            <div class="placeholder">
-            <h3>${data[i].name}</h3>
-            <br />
-            Segment: ${data[i].segment}
-            <br />
-            Scope: ${data[i].scope}
-            </div>
-            `);
-        }
-        // console.log(newHTML);
-        $results.html(newHTML);
-    }
-
-    /**
-     * Appending JSON data to HTML page.
-     * Currently not in use.
-     */
-    function appendJSON(data) {
-        $results.append(`
-                    <div class="placeholder">
-                        <h3>${data.name}</h3>
-                        <br />
-                        Segment: ${data.segment}
-                        <br />
-                        Scope: ${data.scope}
-                    </div>
-                `
-        );
-    }
 }); // END DOCUMENT
