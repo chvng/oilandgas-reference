ed24fff077f346118be7a5facac1394f441ceb41
Chuong Nguyen <32564488+chvng@users.noreply.github.com>
2018-09-13 13:56:49 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,7 +6,8 @@
 $(document).ready(function () {
     // Variables   
     let $results = $('#results');
-    let $segment = $('#segment');
+    let $sector = $('#sector');
+    let $application = $('#application');
     let $scope = $('#scope');
     let $geolocation = $('#geolocation');
 
@@ -50,7 +51,7 @@ $(document).ready(function () {
                                     <img src=${reference.image} class="img--resize">
                                 </div>
                                 <div class="container__text">
-                                    <span class="container__text--header">${reference.name}</span>
+                                    <span class="container__text--h">${reference.name}</span>
                                     <p>${reference.description}</p>
                                 </div>
                             </div>
@@ -71,24 +72,26 @@ $(document).ready(function () {
         let all = "All";
 
         $.getJSON("../references.json", function(data) {
-            $('#segment, #scope, #geolocation').change(function() {
+            $('#sector, #application, #scope, #geolocation').change(function() {
                 // Empty the #results div on change.
                 $results.empty();
 
                 // Running through JSON data.
                 $.each(data, function(i, reference) {
                     // Assign the values to give true when selected is "All".
-                    let segment_value = $segment.val() == all; 
+                    let sector_value = $sector.val() == all;
+                    let application_value = $application.val() == all; 
                     let scope_value = $scope.val() == false;
                     let location_value = $geolocation.val() == all;
 
                     // If the assigned value is not true.. 
-                    if(!segment_value) segment_value = $segment.val() === reference.segment; console.log(segment_value);
+                    if(!sector_value) sector_value = $sector.val() === reference.sector;
+                    if(!application_value) application_value = $application.val() === reference.application;
                     if(!scope_value) scope_value = $scope.val().map(scope => reference.scope.includes(scope)).includes(true);
                     if(!location_value) location_value = $geolocation.val() === reference.location[0] || $geolocation.val() === reference.location[1];
 
                     // Append to HTML whenever the conditions matches.
-                    if(segment_value && scope_value && location_value) appendJSON(reference); 
+                    if(sector_value && application_value && scope_value && location_value) appendJSON(reference); 
                 });
                 rsCounter();
             });
@@ -99,7 +102,7 @@ $(document).ready(function () {
      * Helper function to count the number of ".result"-divs.
      */
     function rsCounter() {
-        let $count = $('#counter');
+        let $count = $('.result-count');
         let div_length = $('.col-3').length;
 
         $count.empty();
@@ -119,7 +122,7 @@ $(document).ready(function () {
                         <img src=${data.image} class="img--resize">
                     </div>
                     <div class="container__text">
-                        <span class="container__text--header">${data.name}</span>
+                        <span class="container__text--h">${data.name}</span>
                         <p>${data.description}</p>
                     </div>
                 </div>
@@ -132,7 +135,11 @@ $(document).ready(function () {
     //=============================================================================//
     //                               STYLING4PRETTY                                //
     //=============================================================================//
-    $segment.select2({
+    $sector.select2({
+        minimumResultsForSearch: -1
+    });
+
+    $application.select2({
         minimumResultsForSearch: -1
     }); 
 
